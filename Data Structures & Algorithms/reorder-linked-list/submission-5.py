161c5d67eadc3acfc12be0f3e0d7a0ccb4f68af6
MD OWAISH RAZA ANSARI <owaishansari492@gmail.com>
2026-06-16 01:52:45 +0530

    Add: reorder-linked-list - submission-5

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-5.py
@@ -0,0 +1,45 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+
+        slow = head
+        fast = head
+        while fast and fast.next:
+
+            # Step 1: Find middle
+
+            slow = slow.next
+            fast = fast.next.next
+
+        # Step 2: Reverse second half
+
+        prev = None
+        curr = slow.next
+        slow.next = None
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+        # Step 3: Merge both halves
+
+        first = head
+        second = prev
+
+        while second:
+            temp1 = first.next
+            temp2 = second.next
+
+            first.next = second
+            second.next = temp1
+
+            first = temp1
+
+            second = temp2
+
